ec1ba70fec2ad32cdece7287ec0a58191ef1a3ff
Nestor Lopez <65617676+NLZ432@users.noreply.github.com>
2022-11-18 15:32:16 -0500

    no name change

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ locals {
 }
 
 resource "azurerm_key_vault" "main" {
-  name                = format("%s-kv", lower(var.name))
+  name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
   tenant_id           = data.azurerm_client_config.current.tenant_id
