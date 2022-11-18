f03c8b8f624788803a37cc48bebdd6237d93e44f
Nestor Lopez <65617676+NLZ432@users.noreply.github.com>
2022-11-18 14:37:18 -0500

    no resource group creation

--- main.tf
+++ main.tf
@@ -32,17 +32,10 @@ locals {
   }
 }
 
-resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
-  location = var.location
-
-  tags = var.tags
-}
-
 resource "azurerm_key_vault" "main" {
   name                = format("%s-kv", lower(var.name))
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = var.sku_name
 
